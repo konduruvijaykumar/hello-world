5b711733ed8b53af5270f9927849b6cb53c852b3
konduruvijaykumar <konduruvijaykumar@gmail.com>
2015-07-31 14:22:42 +0530

    .settings files ignored

--- src/com/vijay/helloworld/HelloWorld.java
+++ src/com/vijay/helloworld/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Say hello to my first git repo...");
+		System.out.println("Say hello to my first git repo....");
 	}
 
 }
