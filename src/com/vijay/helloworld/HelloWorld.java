0010b0e6da31a556186149a46a33bb7ac7efa387
konduruvijaykumar <konduruvijaykumar@gmail.com>
2015-07-31 13:07:00 +0530

    Initial commit

---
+++ src/com/vijay/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.vijay.helloworld;
+
+/**
+ * @author Vijay
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Say hello to my first git repo...");
+	}
+
+}
